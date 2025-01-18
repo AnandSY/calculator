a12520a126b33bedd1f4668775c1143bb6bc212b
AnandSY <anandsy011@gmail.com>
2025-01-18 14:35:16 +0530

    added event listeners to buttons

--- script.js
+++ script.js
@@ -27,6 +27,58 @@ function operate(firstNumber,secondNumber,operator){
     }
 }
 
+function reset(){
+    firstNumber=0,secondNumber=0,operatorClicked=0;
+}
+
+let firstNumber=0, secondNumber=0, operatorArithmetic, operatorAction;
+let operatorClicked = 0;
+
+const digitEvent = document.querySelectorAll(".digits");
+const displayText = document.querySelector(".display");
+const operatorEvent = document.querySelectorAll(".operators");
+
+digitEvent.forEach((element) => {
+    element.addEventListener("click",function(){
+        let number = parseInt(element.textContent);
+        displayText.textContent+=number;
+        // console.log(typeof number);
+        if(operatorClicked===0){
+            firstNumber=firstNumber*10+number;
+        }else{
+            secondNumber=secondNumber*10+number;
+        }
+    });
+});
+
+operatorEvent.forEach(element => {
+    element.addEventListener("click", function(){
+        operatorClicked=1;
+        let operator = element.textContent;
+        // console.log(operator);
+        if(operator!=="Clear" && operator!=="="){
+            operatorArithmetic = operator;
+            // console.log(operatorArithmetic);
+            displayText.textContent+= (" "+operator+" ");
+        }else if(operator==="Clear"){
+            displayText.textContent="";
+            reset();
+        }else if(operator==="="){
+            let result = operate(firstNumber,secondNumber,operatorArithmetic);
+            // console.log(result);
+            displayText.textContent = result.toString();
+            reset();
+            firstNumber=result;
+        }
+    });
+})
+
+
+
+
+
+
+
 // let firstNumber = prompt("Enter the first number");
 // let secondNumber = prompt("Enter the second number");
 // let operator = prompt("Enter the operation you want to use");
